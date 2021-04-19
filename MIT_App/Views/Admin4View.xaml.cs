5536c4535697a7d3c42b5a7d21efef5ec8e2be41
BaoNguyen1001 <81003760+BaoNguyen1001@users.noreply.github.com>
2021-04-19 20:42:05 +0700

    Hoàn thành giao diện Round + Admin(1, 2, 3, 4)

---
+++ MIT_App/Views/Admin4View.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MIT_App.Views
+{
+    /// <summary>
+    /// Interaction logic for Admin4View.xaml
+    /// </summary>
+    public partial class Admin4View : Window
+    {
+        public Admin4View()
+        {
+            InitializeComponent();
+        }
+    }
+}
