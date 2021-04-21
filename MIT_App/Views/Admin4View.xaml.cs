8f6272f3f03bde83cd02842c6c67a51ed9bea736
cutesiucap <72438631+cutesiucap@users.noreply.github.com>
2021-04-21 23:33:32 +0700

    hướng dẫn xử lí sự kiện

--- MIT_App/Views/Admin4View.xaml.cs
+++ MIT_App/Views/Admin4View.xaml.cs
@@ -23,5 +23,30 @@ namespace MIT_App.Views
         {
             InitializeComponent();
         }
+        private async void Answer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Border button = (Border)sender;
+            await Task.Run(() => {
+                Dispatcher.Invoke(() =>
+                {
+                    if (button.Background == Brushes.Transparent)// chưa được chọn -> chọn
+                    {
+                        button.Background = button.BorderBrush;
+                        ((TextBlock)button.Child).Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    }
+                    else // đã được chọn -> hủy chọn
+                    {
+                        button.Background = Brushes.Transparent;
+                        ((TextBlock)button.Child).Foreground = button.BorderBrush;
+                    }
+                });
+
+            });
+
+            /*button.Background = new SolidColorBrush(Color.FromRgb(219, 106, 5));
+            ((TextBlock)answer.Child).Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            AnswerValue = answer.Name.ToString().Trim()[6];*/
+        }
+
     }
 }
