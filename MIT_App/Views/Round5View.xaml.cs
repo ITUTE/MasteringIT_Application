e602a55b1f906d9d540b80c70dfd7e5ddc7bc8dc
BaoNguyen1001 <81003760+BaoNguyen1001@users.noreply.github.com>
2021-05-14 17:45:01 +0700

    Hoàn thành (2/3 Round5View + Admin5View)  + Entity Framework

---
+++ MIT_App/Views/Round5View.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace MIT_App.Views
+{
+    /// <summary>
+    /// Interaction logic for Round5View.xaml
+    /// </summary>
+    public partial class Round5View : Window
+    {
+        public Round5View()
+        {
+            InitializeComponent();
+        }
+
+        public void CutImage()
+        {
+            int count = 0;
+            BitmapImage src = new BitmapImage(new Uri("/Image/avt2_pro.png", UriKind.Relative));
+            Hinh.Source = new CroppedBitmap(src, new Int32Rect(0, 0, 10, 10));
+            
+            //for (int i = 0; i < 3; i++)
+            //    for (int j = 0; j < 3; j++)
+            //        objImg[count++] = new CroppedBitmap(src, new Int32Rect(j * 120, i * 120, 120, 120));
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CutImage();
+        }
+    }
+}
